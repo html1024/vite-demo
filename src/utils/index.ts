0559abe6b294489c6cea5aca05867a4ffbdbdd41
jinlong.yu <jinlong.yu@capgemini.com>
2022-02-20 18:15:08 +0800

    添加工具函数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,3 +12,246 @@ export const getFolder = (path: any) => {
   })
   return components
 }
+
+/**
+ * @description 格式化时间
+ * @param time
+ * @param cFormat
+ * @returns {string|null}
+ */
+export function parseTime(time, cFormat) {
+  if (arguments.length === 0) {
+    return null
+  }
+  const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
+  let date
+  if (typeof time === 'object') {
+    date = time
+  } else {
+    if (typeof time === 'string' && /^[0-9]+$/.test(time)) {
+      time = parseInt(time)
+    }
+    if (typeof time === 'number' && time.toString().length === 10) {
+      time = time * 1000
+    }
+    date = new Date(time)
+  }
+  const formatObj = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay(),
+  }
+  const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+    let value = formatObj[key]
+    if (key === 'a') {
+      return ['日', '一', '二', '三', '四', '五', '六'][value]
+    }
+    if (result.length > 0 && value < 10) {
+      value = '0' + value
+    }
+    return value || 0
+  })
+  return time_str
+}
+
+/**
+ * @description 格式化时间
+ * @param time
+ * @param option
+ * @returns {string}
+ */
+export function formatTime(time, option) {
+  if (('' + time).length === 10) {
+    time = parseInt(time) * 1000
+  } else {
+    time = +time
+  }
+  const d = new Date(time)
+  const now = Date.now()
+
+  const diff = (now - d) / 1000
+
+  if (diff < 30) {
+    return '刚刚'
+  } else if (diff < 3600) {
+    // less 1 hour
+    return Math.ceil(diff / 60) + '分钟前'
+  } else if (diff < 3600 * 24) {
+    return Math.ceil(diff / 3600) + '小时前'
+  } else if (diff < 3600 * 24 * 2) {
+    return '1天前'
+  }
+  if (option) {
+    return parseTime(time, option)
+  }
+  return d.getMonth() + 1 + '月' + d.getDate() + '日' + d.getHours() + '时' + d.getMinutes() + '分'
+}
+
+/**
+ * @description 将url请求参数转为json格式
+ * @param url
+ * @returns {{}|any}
+ */
+export function paramObj(url) {
+  const search = url.split('?')[1]
+  if (!search) {
+    return {}
+  }
+  return JSON.parse(
+    '{"' +
+      decodeURIComponent(search)
+        .replace(/"/g, '\\"')
+        .replace(/&/g, '","')
+        .replace(/=/g, '":"')
+        .replace(/\+/g, ' ') +
+      '"}'
+  )
+}
+
+/**
+ * @description 父子关系的数组转换成树形结构数据
+ * @param data
+ * @returns {*}
+ */
+export function translateDataToTree(data) {
+  const parent = data.filter((value) => value.parentId === 'undefined' || value.parentId == null)
+  const children = data.filter((value) => value.parentId !== 'undefined' && value.parentId != null)
+  const translator = (parent, children) => {
+    parent.forEach((parent) => {
+      children.forEach((current, index) => {
+        if (current.parentId === parent.id) {
+          const temp = JSON.parse(JSON.stringify(children))
+          temp.splice(index, 1)
+          translator([current], temp)
+          typeof parent.children !== 'undefined'
+            ? parent.children.push(current)
+            : (parent.children = [current])
+        }
+      })
+    })
+  }
+  translator(parent, children)
+  return parent
+}
+
+/**
+ * @description 树形结构数据转换成父子关系的数组
+ * @param data
+ * @returns {[]}
+ */
+export function translateTreeToData(data) {
+  const result = []
+  data.forEach((item) => {
+    const loop = (data) => {
+      result.push({
+        id: data.id,
+        name: data.name,
+        parentId: data.parentId,
+      })
+      const child = data.children
+      if (child) {
+        for (let i = 0; i < child.length; i++) {
+          loop(child[i])
+        }
+      }
+    }
+    loop(item)
+  })
+  return result
+}
+
+/**
+ * @description 获取随机id
+ * @param length
+ * @returns {string}
+ */
+export function uuid(length = 32) {
+  const num = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  let str = ''
+  for (let i = 0; i < length; i++) {
+    str += num.charAt(Math.floor(Math.random() * num.length))
+  }
+  return str
+}
+
+/**
+ * @description m到n的随机数
+ * @param m
+ * @param n
+ * @returns {number}
+ */
+export function random(m, n) {
+  return Math.floor(Math.random() * (m - n) + n)
+}
+
+/**
+ * @description addEventListener
+ * @type {function(...[*]=)}
+ */
+export const on = (function () {
+  return function (element, event, handler, useCapture = false) {
+    if (element && event && handler) {
+      element.addEventListener(event, handler, useCapture)
+    }
+  }
+})()
+
+/**
+ * @description removeEventListener
+ * @type {function(...[*]=)}
+ */
+export const off = (function () {
+  return function (element, event, handler, useCapture = false) {
+    if (element && event) {
+      element.removeEventListener(event, handler, useCapture)
+    }
+  }
+})()
+// 获取对象属性值
+export const _g = function (list, param, defualtStr = '') {
+  let param2, result
+  if (list && Array.isArray(param) && param.length > 0) {
+    param2 = param.slice(1)
+    result = list[param[0]]
+    if (param.length > 1) {
+      result = _g(result, param2, defualtStr)
+    }
+  } else {
+    result = list || defualtStr
+  }
+  return result
+}
+// https://mock.apipost.cn/app/mock/project/f8fb9938-0f04-4905-baf7-f5bc28384327/customer/getList
+// let fieldName = _g(res,['result','pagination','dataSort','fieldName'],'-')
+// 根据模板组装列表下拉数据
+export const _getTemplate = function (node, temp, defualtStr = '') {
+  let result = temp,
+    t = /(#)([^#]+)(#)/.exec(temp)
+  if (t && t.length) {
+    let key = t[2].split('||')[0]
+    let def = t[2].split('||')[1] === undefined ? defualtStr : t[2].split('||')[1]
+    let res = (key && node[key]) || def
+    result = result.replace(t[0], res)
+    result = _getTemplate(node, result)
+  }
+  return result
+}
+// _getTemplate(_g(res,['result','pagination']),'maxResults:#maxResults#-----pageCount:#pageCount#=====recordCount #recordCount1||电饭锅#')
+export const optionalChaining = (obj, str = '', ...rest) => {
+  let tmp = obj
+  for (let key in rest) {
+    let name = rest[key]
+    if (tmp?.[name] === undefined) {
+      tmp = str
+      break
+    } else {
+      tmp = tmp?.[name]
+    }
+  }
+  return tmp
+}
+// optionalChaining(res,'kkk','result','pagination','dataSort','fieldName')
